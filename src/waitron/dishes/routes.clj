b799c94963b5e34d42fcc596539479cb53253d61
Pablo Bastidas <pablobastidasv@gmail.com>
2024-04-14 13:57:47 +0200

    fixing small format

--- src/waitron/dishes/routes.clj
+++ src/waitron/dishes/routes.clj
@@ -12,6 +12,5 @@
         :post handlers/handle-post-dishes :middleware [wrap-json-body]}]
    ["/list" {:get handlers/handle-get-dishes-table}]
    ["/new" {:get handlers/handle-get-new-dish}]
-   ["/:id" {:get handlers/handle-get-dish-by-id-page}]
-   ])
+   ["/:id" {:get handlers/handle-get-dish-by-id-page}]])
 
